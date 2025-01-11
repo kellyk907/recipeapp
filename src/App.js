15a95e77d1235be876c44c6ae9c625b412c96b69
kelly_k <pinkflamingo681@gmail.com>
2025-01-11 15:50:21 -0500

    allow users to add their own recipes to site through filling out a form + frontend dev.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import Header from "./components/Header";
-import RecipeExcerpt from "./components/RecipeExerpt";
 import RecipeFull from "./components/RecipeFull";
+import RecipeExcerpt from "./components/RecipeExerpt";
+import NewRecipeForm from "./components/NewRecipeForm";
 import "./App.css";
 
 function App() {
   const [recipes, setRecipes] = useState([]);
   const [selectedRecipe, setSelectedRecipe] = useState(null);
+  const [showNewRecipeForm, setShowNewRecipeForm] = useState(false);
+
+  const [newRecipe, setNewRecipe] = useState({
+    title: "",
+    ingredients: "",
+    instructions: "",
+    servings: 1,
+    description: "",
+    image_url: "https://images.pexels.com/photos/9986228/pexels-photo-9986228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  });
 
   useEffect(() => {
     const fetchAllRecipes = async () => {
@@ -17,11 +28,10 @@ function App() {
           const data = await response.json();
           setRecipes(data);
         } else {
-          console.log("Oops - could not fetch recipes!", "error");
+          console.log("Oops - could not fetch recipes!");
         }
       } catch (e) {
         console.error("An error occurred during the request:", e);
-        console.log("An unexpected error occurre  d. Please try again later.", "error");
       }
     };
     fetchAllRecipes();
@@ -35,9 +45,24 @@ function App() {
     setSelectedRecipe(null);
   };
 
+  const hideRecipeForm = () => {
+    setShowNewRecipeForm(false);
+  };
+
+  const showRecipeForm = () => {
+    setShowNewRecipeForm(true);
+    setSelectedRecipe(null);
+  };
+
+  const onUpdateForm = (e) => {
+    const { name, value } = e.target;
+    setNewRecipe({ ...newRecipe, [name]: value });
+  };
+
   return (
     <div className='recipe-app'>
-      <Header />
+      <Header showRecipeForm={showRecipeForm} />
+      {showNewRecipeForm && <NewRecipeForm newRecipe={newRecipe} hideRecipeForm={hideRecipeForm} onUpdateForm={onUpdateForm} />}
       {selectedRecipe && <RecipeFull selectedRecipe={selectedRecipe} handleUnselectRecipe={handleUnselectRecipe} />}
       {!selectedRecipe && (
         <div className='recipe-list'>
