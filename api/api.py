80c4c0b4dc12978dab933f591901b776992ca05f
kelly_k <pinkflamingo681@gmail.com>
2025-01-11 14:58:18 -0500

    completed API CRUD with Flask and ThunderClient,allowing for a a very intuitive interface

--- api/api.py
+++ api/api.py
@@ -1,3 +1,4 @@
+import time
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
@@ -20,12 +21,13 @@ class Recipe(db.Model):
     def __repr__(self):
         return f"Recipe(id={self.id}, title='{self.title}', description='{self.description}', servings={self.servings})"
 
-
-with app.app_context():
-    db.create_all()
-    db.session.commit()
+# with app.app_context():
+#     db.create_all()
+#     db.session.commit()
 
 # Route to fetch all recipes
+
+
 @app.route('/api/recipes', methods=['GET'])
 def get_all_recipes():
     recipes = Recipe.query.all()
@@ -43,6 +45,8 @@ def get_all_recipes():
     return jsonify(recipe_list)
 
 # Route to add a new recipe
+
+
 @app.route('/api/recipes', methods=['POST'])
 def add_recipe():
     data = request.get_json()
@@ -78,6 +82,60 @@ def add_recipe():
     }
 
     return jsonify({'message': 'Recipe added successfully', 'recipe': new_recipe_data})
+
+# Route to update a recipe
+
+
+@app.route('/api/recipes/<int:recipe_id>', methods=['PUT'])
+def update_recipe(recipe_id):
+    recipe = Recipe.query.get(recipe_id)
+    if not recipe:
+        return jsonify({'error': 'Recipe not found'}), 404
+
+    data = request.get_json()
+
+    # Validate the incoming JSON data for required fields
+    required_fields = ['title', 'ingredients',
+                       'instructions', 'servings', 'description', 'image_url']
+
+    for field in required_fields:
+        if field not in data or data[field] == "":
+            return jsonify({'error': f"Missing required field: '{field}'"}), 400
+
+    recipe.title = data['title']
+    recipe.ingredients = data['ingredients']
+    recipe.instructions = data['instructions']
+    recipe.servings = data['servings']
+    recipe.description = data['description']
+    recipe.image_url = data['image_url']
+
+    db.session.commit()
+ # Serialize the updated recipe and return it as JSON
+    updated_recipe = {
+        'id': recipe.id,
+        'title': recipe.title,
+        'ingredients': recipe.ingredients,
+        'instructions': recipe.instructions,
+        'servings': recipe.servings,
+        'description': recipe.description,
+        'image_url': recipe.image_url
+    }
+
+    return jsonify({'message': 'Recipe updated successfully', 'recipe': updated_recipe})
+
+# Route to delete a recipe
+
+
+@app.route('/api/recipes/<int:recipe_id>', methods=['DELETE'])
+def delete_recipe(recipe_id):
+    recipe = Recipe.query.get(recipe_id)
+    if not recipe:
+        return jsonify({'error': 'Recipe not found'}), 404
+
+    db.session.delete(recipe)
+    db.session.commit()
+    return jsonify({'message': 'Recipe deleted successfully'})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
